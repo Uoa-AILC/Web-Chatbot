b12b5fb077a9aa828e15ef1f0b980663a9cbb23d
xche529 <xche529@aucklanduni.ac.nz>
2024-05-25 23:38:53 +1200

    feat: add start files

---
+++ index.js
@@ -0,0 +1,44 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { HarmBlockThreshold, HarmCategory } = require("@google/generative-ai");
+require("dotenv").config();
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function run() {
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+
+  const generationConfig = {
+    stopSequences: ["red"],
+    maxOutputTokens: 500,
+    temperature: 0.1,
+    topP: 0.1,
+    topK: 16,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+  ];
+
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    safetySettings,
+    generationConfig,
+  });
+
+  const prompt = "intro: You are a chatbot please help the user with their question";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
